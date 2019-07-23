e7cb55bc43c3f063f2227ba859ce90e8f20dd26e
Daniel Macedo <admacedo@gmail.com>
2019-07-23 15:56:08 +0100

    Release

--- sw.js
+++ sw.js
@@ -2,28 +2,28 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0618ed6ecc8bdfb542ea.js",
-    "revision": "1f62dfdb6b298fee1095b3d9d3dda9dc"
+    "url": "/_nuxt/5442ee39fe6c9e403b5b.js",
+    "revision": "4573eef1706294b8a43ffdacf9a285e2"
   },
   {
-    "url": "/_nuxt/3fe183c341365519e477.js",
-    "revision": "03875e55e504cc94ab6614e77206245b"
+    "url": "/_nuxt/7ab3ba4556e0c30ac8d5.js",
+    "revision": "3b57bcafadb002245430b44b378fe27a"
   },
   {
-    "url": "/_nuxt/73bc7f5a9538c101cd04.js",
-    "revision": "292f16207955666ff51446eabb577cf0"
+    "url": "/_nuxt/a390728b82b2a31a27c8.js",
+    "revision": "4cd46db4f5ab40c2ea6bf5823af210b5"
   },
   {
-    "url": "/_nuxt/7e173d337375c4f89804.js",
-    "revision": "2ebf2356f8fd75af243f20f761cf0920"
+    "url": "/_nuxt/bc5bfaf387497e25ecce.js",
+    "revision": "68579cedc1bb209c5fb5dc3b99807211"
   },
   {
-    "url": "/_nuxt/8922992799cc7d96b661.js",
-    "revision": "53780ccfd71f0ca177c5c901c79f6e5f"
+    "url": "/_nuxt/f4145b9e2502e63e1d2e.js",
+    "revision": "e85e26cccdbd5bbfe1ab8ed2aa600a02"
   },
   {
-    "url": "/_nuxt/97d8c1e37e55b3b36169.js",
-    "revision": "3d260c5a48ecdd0e618dbd9470c494a7"
+    "url": "/_nuxt/f7a1e8b8a7bbc5ecfbc5.js",
+    "revision": "1c9051bb51510842c5f5a64a7adf3b73"
   }
 ], {
   "cacheId": "cityjsconf",
