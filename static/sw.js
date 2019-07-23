9069284f83f3e86fa8bc4abed1ef3fe8ded0f1ea
Daniel Macedo <admacedo@gmail.com>
2019-07-23 16:28:06 +0100

    Add CircleCI

--- static/sw.js
+++ static/sw.js
@@ -2,28 +2,28 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/2023c38235c6d927a52a.js",
-    "revision": "c58984866aeaf5f3664e4b88bf74ae3b"
-  },
-  {
-    "url": "/_nuxt/3a07042bbc8485eef5fb.js",
-    "revision": "a530625f1b672a761407a127a6529674"
-  },
-  {
-    "url": "/_nuxt/4f110acfa8976e4f4d13.js",
-    "revision": "d24e78bbf8b2ef9ca884b101d4349243"
-  },
-  {
     "url": "/_nuxt/5442ee39fe6c9e403b5b.js",
     "revision": "4573eef1706294b8a43ffdacf9a285e2"
   },
   {
-    "url": "/_nuxt/54b2d531b0665fc0b9f8.js",
-    "revision": "3f7f9d6ff3ed020107260021292938cd"
+    "url": "/_nuxt/9fa013c252b323edfdae.js",
+    "revision": "c2bae17b5c47a78305ccdd326964407d"
   },
   {
-    "url": "/_nuxt/c61a73dc182e386981aa.js",
-    "revision": "0bb4fb5dce6733490b0a004287019029"
+    "url": "/_nuxt/b3100877afcd7333120b.js",
+    "revision": "02b0d17617a46cd5a462d19cdeec11fd"
+  },
+  {
+    "url": "/_nuxt/bc5bfaf387497e25ecce.js",
+    "revision": "68579cedc1bb209c5fb5dc3b99807211"
+  },
+  {
+    "url": "/_nuxt/f4145b9e2502e63e1d2e.js",
+    "revision": "e85e26cccdbd5bbfe1ab8ed2aa600a02"
+  },
+  {
+    "url": "/_nuxt/f7a1e8b8a7bbc5ecfbc5.js",
+    "revision": "1c9051bb51510842c5f5a64a7adf3b73"
   }
 ], {
   "cacheId": "cityjsconf",
